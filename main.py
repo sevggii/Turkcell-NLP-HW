0e2e405144f58f7aaea491a876a6bce08ac80876
sevggii <sevgitr_@hotmail.com>
2025-07-05 12:09:17 +0300

    csv dosyaları eklendi

---
+++ main.py
@@ -0,0 +1,8 @@
+# https://github.com/google-research/google-research/tree/master/goemotions/data/full_dataset
+# Buradkai veri setiyle metin işleme yaparak. Gelen yorumdan o yorumdaki genel duygu tutumunu tahmin eden modeli geliştirelim.
+# 1. fark => Sınıflandırma => 27 farklı duygu türü
+
+
+
+####3 csv dosyası var. birleştirip, tek csv dosyası oluştur. (yazar adı link vs duygu deeğerleri)
+#teslim hafta perşembe 10 Temmuz
